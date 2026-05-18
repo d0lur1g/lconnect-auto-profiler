483b63d1621d93ac8b7fb19f05f112ac01bd9ff5
Ludovic Girard <d0lur1g@users.noreply.github.com>
2026-05-19 01:32:05 +0200

    refactor: remove MergeLightingSetting (replaced by List<LightingSetting>)

--- LConnect.AutoProfiler.Core/Models/MergeLightingSetting.cs
+++
@@ -1,9 +0,0 @@
-namespace LConnect.AutoProfiler.Core.Models;
-
-public class MergeLightingSetting
-{
-    public int Mode       { get; set; }
-    public int Speed      { get; set; }
-    public int Brightness { get; set; }
-    public int Direction  { get; set; }
-}
